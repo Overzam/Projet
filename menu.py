193c8f14dfcd94c678e771581e5e67fac40d3b27
aioIia <73951461+aioIia@users.noreply.github.com>
2022-05-22 21:58:39 +0200

    lumiere

--- menu.py
+++ menu.py
@@ -25,6 +25,12 @@ def base_menu():
     options_but = pyg.transform.scale(options_but, (height // 5, width // 5)).convert_alpha()
     quit_but = pyg.image.load("img_dylan/quit_butt.png")
     quit_but = pyg.transform.scale(quit_but, (height // 4, width // 5)).convert_alpha()
+    play_but_hover = pyg.image.load("img_dylan/jouerwhite.png")
+    play_but_hover = pyg.transform.scale(play_but_hover, (height // 5, width // 5)).convert_alpha()
+    options_but_hover = pyg.image.load("img_dylan/testwhite.png")
+    options_but_hover = pyg.transform.scale(options_but_hover, (height // 5, width // 5)).convert_alpha()
+    quit_but_hover = pyg.image.load("img_dylan/quitterwhite.png")
+    quit_but_hover = pyg.transform.scale(quit_but_hover, (height // 4, width // 5)).convert_alpha()
 
     class button:
         def __init__(self, x, y, image):
@@ -35,7 +41,6 @@ def base_menu():
 
         def draw(self):
             action = False
-
             pos = pyg.mouse.get_pos()
 
             if self.rect.collidepoint(pos):
@@ -43,21 +48,45 @@ def base_menu():
                     if evenement.type == pyg.MOUSEBUTTONUP and self.clicked == False:
                         self.clicked = True
                         action = True
-
             screen.blit(self.image, (self.rect.x, self.rect.y))
+
             return action
+
 
     play_but = button(height // 2 - 200, 200, play_but)
     options_but = button(height // 2 - 200, 500, options_but)
     quit_but = button(height // 2 - 240, 800, quit_but)
 
+    play = pyg.image.load("img_dylan/play_but.png")
+    play = pyg.transform.scale(play, (height // 5, width // 5)).convert_alpha()
+    options = pyg.image.load("img_dylan/option_but.png")
+    options = pyg.transform.scale(options, (height // 5, width // 5)).convert_alpha()
+    quit = pyg.image.load("img_dylan/quit_butt.png")
+    quit = pyg.transform.scale(quit, (height // 4, width // 5)).convert_alpha()
+
     font = pyg.font.Font('freesansbold.ttf', 128)
-    titre = "Wario Ware"
+    titre = "Hagini sauce"
     texte = font.render(str(titre), True, white, black)
     screen.blit(texte, (height // 2 - 350, 50))
     pyg.display.update()
 
     while True:
+        mouse = pyg.mouse.get_pos()
+        if mouse[0] > height // 2 - 200 and mouse[0] < height // 2 - 200 + height // 5 and mouse[1] > 200 and mouse[1] < 200 + width // 5:
+            play_but = button(height // 2 - 200, 200, play_but_hover)
+        else:
+            play_but = button(height // 2 - 200, 200, play)
+
+        if mouse[0] > height // 2 - 200 and mouse[0] < height // 2 - 200 + height // 5 and mouse[1] > 500 and mouse[1] < 500 + width // 5:
+            options_but = button(height // 2 - 200, 500, options_but_hover)
+        else:
+            options_but = button(height // 2 - 200, 500, options)
+
+        if mouse[0] > height // 2 - 200 and mouse[0] < height // 2 - 200 + height // 5 and mouse[1] > 800 and mouse[1] < 800 + width // 5:
+            quit_but = button(height // 2 - 200, 800, quit_but_hover)
+        else:
+            quit_but = button(height // 2 - 200, 800, quit)
+
         if play_but.draw():
             musique_menu.stop()
             gamemode = 0
